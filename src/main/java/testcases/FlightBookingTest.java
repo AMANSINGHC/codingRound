d2149dc2a3242ad7b6cd5f2cc80d488e96c4fceb
Aman Singh Chauhan <Aman Singh Chauhan@169.254.106.13>
2018-03-30 13:24:27 +0530

    Did changes in the testng.xml file so that the 3 tests run simultaneously. This will reduce the execution time to run the suite.

--- src/main/java/testcases/FlightBookingTest.java
+++ src/main/java/testcases/FlightBookingTest.java
@@ -1,18 +1,18 @@
 package testcases;
-import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
-import com.sun.javafx.PlatformUtil;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
+import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
-import java.util.List;
+import com.sun.javafx.PlatformUtil;
 
+@SuppressWarnings("restriction")
 public class FlightBookingTest {
 
     WebDriver driver;
@@ -24,7 +24,9 @@ public class FlightBookingTest {
         setDriverPath();
         driver = new ChromeDriver();
         driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        
         driver.findElement(By.id("OneWay")).click();
 
         driver.findElement(By.id("FromTag")).clear();
@@ -32,26 +34,25 @@ public class FlightBookingTest {
 
         //wait for the auto complete options to appear for the origin
 
-        waitFor(2000);
+        
         List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
         originOptions.get(0).click();
 
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
+        driver.findElement(By.id("ToTag")).clear();
+        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
 
         //wait for the auto complete options to appear for the destination
 
-        waitFor(2000);
+        
         //select the first item from the destination auto complete list
         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
         destinationOptions.get(0).click();
 
-        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
+        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[2]/table/tbody/tr[3]/td[7]/a")).click();
 
         //all fields filled in. Now click on search
         driver.findElement(By.id("SearchBtn")).click();
 
-        waitFor(5000);
         //verify that result appears for the provided journey search
         AssertJUnit.assertTrue(isElementPresent(By.className("searchSummary")));
 
@@ -59,16 +60,6 @@ public class FlightBookingTest {
         driver.quit();
 
     }
-
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
 
     private boolean isElementPresent(By by) {
         try {
@@ -81,13 +72,13 @@ public class FlightBookingTest {
 
     private void setDriverPath() {
         if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//libs//chromedriver");
         }
         if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//libs//chromedriver.exe");
         }
         if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//libs//chromedriver_linux");
         }
     }
 }
